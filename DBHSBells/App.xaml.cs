5152db101fc3293afe74164b2be33c0745c7992f
Andy Liu <andyliu0330@gmail.com>
2024-02-05 12:18:17 -0800

    Add project files.

---
+++ DBHSBells/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DBHSBells
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DBHSBells/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DBHSBells
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
